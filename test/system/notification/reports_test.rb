24cf8596b76e874987fe33b525455c6ee7a5d145
kaisumi <kaisumi@langue-lab.com>
2022-01-21 15:56:17 +0900

    テストを修正（成功するはず）

--- test/system/notification/reports_test.rb
+++ test/system/notification/reports_test.rb
@@ -171,6 +171,7 @@ class Notification::ReportsTest < ApplicationSystemTestCase
     all('.learning-time')[0].all('.learning-time__finished-at select')[0].select('08')
     all('.learning-time')[0].all('.learning-time__finished-at select')[1].select('30')
     click_button '提出'
+    find('.modal-header__close').click
 
     click_link '日報作成'
     within('#new_report') do
@@ -184,6 +185,7 @@ class Notification::ReportsTest < ApplicationSystemTestCase
     all('.learning-time')[0].all('.learning-time__finished-at select')[0].select('08')
     all('.learning-time')[0].all('.learning-time__finished-at select')[1].select('30')
     click_button '提出'
+    find('.modal-header__close').click
 
     logout
 
